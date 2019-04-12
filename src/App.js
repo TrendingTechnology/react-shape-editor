8218075a6fcc0d36e32acf6c9e87d367e0ef9633
fritz-c <4413963+fritz-c@users.noreply.github.com>
2019-04-12 18:18:12 +0900

    chore: sample picture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ShapeEditor from './ShapeEditor';
 import SingleLineShape from './SingleLineShape';
-import formImage from './blank.jpg';
+import bgImage from './blank.png';
 
 let iterator = 0;
 
@@ -19,7 +19,7 @@ class App extends Component {
 
     this.state = {
       scale: 0.75,
-      items: [{ id: 1, x: 92, y: 200, width: 150, height: 125 }],
+      items: [{ id: 1, x: 302, y: 200, width: 150, height: 125 }],
     };
   }
 
@@ -35,68 +35,84 @@ class App extends Component {
     const to5 = n => Math.floor(n / 5) * 5;
 
     return (
-      <div style={{ height: 400, paddingLeft: 20 }}>
+      <div>
         <button onClick={() => changeScale(1 / Math.sqrt(2))}>-</button>
         <button onClick={() => changeScale(Math.sqrt(2))}>+</button>
-        <br />
-        <ShapeEditor
-          planeImageSrc={formImage}
-          scale={scale}
-          onAddShape={({ x, y, width, height }) => {
-            this.setState(state => ({
-              items: [
-                ...state.items,
-                { id: `id${iterator}`, x, y, width, height },
-              ],
-            }));
-            iterator += 1;
-          }}
-          constrainMove={({ x, y, width, height, planeWidth, planeHeight }) => {
-            return {
-              x: to5(Math.min(planeWidth - width, Math.max(0, x))),
-              y: to5(Math.min(planeHeight - height, Math.max(0, y))),
-            };
-          }}
-          constrainResize={({
-            startCorner: { x: startX, y: startY },
-            movingCorner: { x: movingX, y: movingY },
-            planeWidth,
-            planeHeight,
-          }) => {
-            return {
-              x: to5(Math.min(planeWidth, Math.max(0, movingX))),
-              y: to5(Math.min(planeHeight, Math.max(0, movingY))),
-            };
+
+        <div
+          style={{
+            height: 400,
+            margin: 20,
+            border: 'solid gray 2px',
+            boxSizing: 'border-box',
           }}
         >
-          {items.map((item, index) => {
-            const { id, width, height, x, y, type, ...otherProps } = item;
-            return (
-              <SingleLineShape
-                key={id}
-                width={width}
-                height={height}
-                x={x}
-                y={y}
-                keyboardTransformMultiplier={5}
-                onChange={newRect => {
-                  this.setState(state => ({
-                    items: arrayReplace(state.items, index, {
-                      ...item,
-                      ...newRect,
-                    }),
-                  }));
-                }}
-                onDelete={() => {
-                  this.setState(state => ({
-                    items: arrayReplace(state.items, index, []),
-                  }));
-                }}
-                {...otherProps}
-              />
-            );
-          })}
-        </ShapeEditor>
+          <ShapeEditor
+            planeImageSrc={bgImage}
+            scale={scale}
+            onAddShape={({ x, y, width, height }) => {
+              this.setState(state => ({
+                items: [
+                  ...state.items,
+                  { id: `id${iterator}`, x, y, width, height },
+                ],
+              }));
+              iterator += 1;
+            }}
+            constrainMove={({
+              x,
+              y,
+              width,
+              height,
+              planeWidth,
+              planeHeight,
+            }) => {
+              return {
+                x: to5(Math.min(planeWidth - width, Math.max(0, x))),
+                y: to5(Math.min(planeHeight - height, Math.max(0, y))),
+              };
+            }}
+            constrainResize={({
+              startCorner: { x: startX, y: startY },
+              movingCorner: { x: movingX, y: movingY },
+              planeWidth,
+              planeHeight,
+            }) => {
+              return {
+                x: to5(Math.min(planeWidth, Math.max(0, movingX))),
+                y: to5(Math.min(planeHeight, Math.max(0, movingY))),
+              };
+            }}
+          >
+            {items.map((item, index) => {
+              const { id, width, height, x, y, type, ...otherProps } = item;
+              return (
+                <SingleLineShape
+                  key={id}
+                  width={width}
+                  height={height}
+                  x={x}
+                  y={y}
+                  keyboardTransformMultiplier={5}
+                  onChange={newRect => {
+                    this.setState(state => ({
+                      items: arrayReplace(state.items, index, {
+                        ...item,
+                        ...newRect,
+                      }),
+                    }));
+                  }}
+                  onDelete={() => {
+                    this.setState(state => ({
+                      items: arrayReplace(state.items, index, []),
+                    }));
+                  }}
+                  {...otherProps}
+                />
+              );
+            })}
+          </ShapeEditor>
+        </div>
       </div>
     );
   }
